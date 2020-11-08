6dc979ce79928dfb3fcff9f5fa2da885e15c10ad
Andrey <kouznts@ya.ru>
2020-11-08 16:04:28 +0400

    impl file reader thread run

--- lab2/src/FileReaderThread.java
+++ lab2/src/FileReaderThread.java
@@ -21,4 +21,43 @@ class FileReaderThread extends Thread {
     public FileReaderThread(String filePathName) {
         this.filePathName = filePathName;
     }
+
+    public void run() {
+
+        /* для получения блокировки вызывается метод lock(),
+         * а после окончания работы с общими ресурсами вызывается метод unlock(), который снимает блокировку: */
+        locker.lock();
+        try {
+
+            final Scanner scanner = new Scanner(new File(filePathName));
+            while (true) {
+                if (!isMyTurn) {
+                    /* поток ожидает, пока не будет выполнено некоторое условие
+                     * и пока другой поток не вызовет методы signal/signalAll: */
+                    readingNextLine.await();
+                }
+
+                if (scanner.hasNextLine()) {
+                    currLine = scanner.nextLine();
+                } else {
+                    currLine = null;
+                }
+
+                isMyTurn = false;
+
+                if (isMyWorkFinished) {
+                    scanner.close();
+                    break;
+                }
+
+                /* сигнализирует, что поток, у которого ранее был вызван метод await(), может продолжить работу,
+                 * важно в конце вызвать метод signal/signalAll, чтобы избежать возможности взаимоблокировки потоков: */
+                readingNextLineIsFinished.signal();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            locker.unlock();
+        }
+    }
 }
