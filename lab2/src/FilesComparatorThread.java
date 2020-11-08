c707215242cde6234ff3cbdc4b627ae44f354b94
Andrey <kouznts@ya.ru>
2020-11-08 17:00:54 +0400

    impl FCT sequential run

--- lab2/src/FilesComparatorThread.java
+++ lab2/src/FilesComparatorThread.java
@@ -57,4 +57,29 @@ public class FilesComparatorThread extends Thread {
             currStringsNum++;
         }
     }
+
+    private void runSequential() {
+        String file1Line;
+        String file2Line;
+        boolean isEndOfFiles;
+        int currStringsNum = 1;
+
+        while (true) {
+            file1.runReadLine();
+            file1Line = file1.currLine();
+            file2.runReadLine();
+            file2Line = file2.currLine();
+
+            isEndOfFiles = file1Line == null & file2Line == null;
+            if (isEndOfFiles) {
+                break;
+            }
+
+            if (!Objects.equals(file1Line, file2Line)) {
+                System.out.println("Number " + currStringsNum + ": line 1 = " + file1Line + ", line 2 = " + file2Line);
+            }
+
+            currStringsNum++;
+        }
+    }
 }
