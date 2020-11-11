5e5a09bf5be60cfd120a119e1d2b6e110fa3704a
Andrey <kouznts@ya.ru>
2020-11-11 12:02:40 +0400

    add thread numbers static field

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
 
-    private static final int LAST_NUMBER = 100_000;
+    private static final int LAST_NUMBER = 200_000_000;
+    private static final int THREADS_NUM = 56;
 
     public static void main(String[] args) {
 
         SequentialFindingPrimes.primes(LAST_NUMBER);
-        ParallelFindingPrimes.primes(LAST_NUMBER, 2);
+        ParallelFindingPrimes.primes(LAST_NUMBER, THREADS_NUM);
     }
 }
