ac111ea7e42202e30ee62560738d747264afff61
Andrey <kouznts@ya.ru>
2020-11-11 16:42:13 +0400

    check mass out of bounds

--- lab4/src/nbody/Body.java
+++ lab4/src/nbody/Body.java
@@ -1,6 +1,11 @@
 package nbody;
 
+import nbody.exceptions.MOutOfBoundsException;
+
 public class Body {
+
+    private static final double MIN_M = 0.1;
+    private static final double MAX_M = 100;
 
     private final Point p;
     private final Point v;
@@ -11,10 +16,15 @@ public class Body {
         p = new Point(0.1, 0.1);
         v = new Point(0.1, 0.1);
         f = new Point(0.1, 0.1);
-        m = 0.1;
+        m = MIN_M;
     }
 
     public Body(double m) {
+
+        if (m < MIN_M || m > MAX_M) {
+            throw new MOutOfBoundsException();
+        }
+
         p = new Point(0.1, 0.1);
         v = new Point(0.1, 0.1);
         f = new Point(0.1, 0.1);
