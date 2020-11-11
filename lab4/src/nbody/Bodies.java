21a2684481614a120af6d701158a23028c8c279b
Andrey <kouznts@ya.ru>
2020-11-11 17:27:08 +0400

    impl static bodies class

---
+++ lab4/src/nbody/Bodies.java
@@ -0,0 +1,40 @@
+package nbody;
+
+public class Bodies {
+
+    public static final double G = 6.67e-11;
+
+    public static final double MIN_M = 0.1;
+    public static final double MAX_M = 100;
+
+    public static double distance(Body b1, Body b2) {
+        return Math.sqrt(
+                Math.pow(b1.p().x() - b2.p().x(), 2) + Math.pow(b1.p().y() - b2.p().y(), 2)
+        );
+    }
+
+    public static double magnitude(Body b1, Body b2, double b1b2distance) {
+        return G * b1.m() * b2.m() / Math.pow(b1b2distance, 2);
+    }
+
+    public static Point direction(Body b1, Body b2) {
+        return new Point(
+                b2.p().x() - b1.p().x(),
+                b2.p().y() - b1.p().y()
+        );
+    }
+
+    public static Point dv(Body b, long dt) {
+        return new Point(
+                b.f().x() / b.m() * dt,
+                b.f().y() / b.m() * dt
+        );
+    }
+
+    public static Point dp(Body b, long dt, Point dv) {
+        return new Point(
+                (b.v().x() + dv.x() / 2) * dt,
+                (b.v().y() + dv.y() / 2) * dt
+        );
+    }
+}
