8350abb032f5611eed0c450ec55f22bfa9d8350c
Andrey <kouznts@ya.ru>
2020-11-11 14:31:25 +0400

    check N bounds

--- lab4/src/nbody/NbodySolver.java
+++ lab4/src/nbody/NbodySolver.java
@@ -1,10 +1,20 @@
 package nbody;
 
+import nbody.exceptions.NOutOfBoundsException;
+
 public class NbodySolver {
+
+    public static int MIN_N = 1;
+    public static int MAX_N = 5;
 
     public final int N; // публичное поле, потому что константа
 
-    public  NbodySolver(int N) {
+    public NbodySolver(final int N) {
+
+        if (N < MIN_N || N > MAX_N) {
+            throw new NOutOfBoundsException();
+        }
+
         this.N = N;
     }
 }
