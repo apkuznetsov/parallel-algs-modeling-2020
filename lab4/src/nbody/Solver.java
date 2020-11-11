a41f0052967c84f62068a1717ebc6c26e04f9980
Andrey <kouznts@ya.ru>
2020-11-11 14:31:56 +0400

    rename solver

--- lab4/src/nbody/Solver.java
+++ lab4/src/nbody/Solver.java
@@ -2,14 +2,14 @@ package nbody;
 
 import nbody.exceptions.NOutOfBoundsException;
 
-public class NbodySolver {
+public class Solver {
 
     public static int MIN_N = 1;
     public static int MAX_N = 5;
 
     public final int N; // публичное поле, потому что константа
 
-    public NbodySolver(final int N) {
+    public Solver(final int N) {
 
         if (N < MIN_N || N > MAX_N) {
             throw new NOutOfBoundsException();
