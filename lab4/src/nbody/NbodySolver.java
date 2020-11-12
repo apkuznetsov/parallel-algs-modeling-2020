c918ec0702f9a857b96f202f8c319c43bd170664
Andrey <kouznts@ya.ru>
2020-11-12 13:13:42 +0400

    fix bounds bug

--- lab4/src/nbody/NbodySolver.java
+++ lab4/src/nbody/NbodySolver.java
@@ -55,8 +55,8 @@ public class NbodySolver {
         Point direction;
 
         final int N = b.length;
-        for (int i = 1; i <= N - 1; i++) {
-            for (int j = i + 1; j <= N; j++) {
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
                 distance = distance(b[i], b[j]);
                 magnitude = magnitude(b[i], b[j], distance);
                 direction = direction(b[i], b[j]);
@@ -78,21 +78,21 @@ public class NbodySolver {
         Point dv; // dv = f/m * dt
         Point dp; // dp = (v + dv/2) * dt
 
-        for (int i = 1; i <= b.length; i++) {
-            dv = dv(b[i], dt);
-            dp = dp(b[i], dt, dv);
+        for (Body body : b) {
+            dv = dv(body, dt);
+            dp = dp(body, dt, dv);
 
-            b[i].setV(
-                    b[i].v().x() + dv.x(),
-                    b[i].v().y() + dv.y()
+            body.setV(
+                    body.v().x() + dv.x(),
+                    body.v().y() + dv.y()
             );
 
-            b[i].setP(
-                    b[i].p().x() + dp.x(),
-                    b[i].p().y() + dp.y()
+            body.setP(
+                    body.p().x() + dp.x(),
+                    body.p().y() + dp.y()
             );
 
-            b[i].setF(0.0, 0.0);
+            body.setF(0.0, 0.0);
         }
     }
 }
